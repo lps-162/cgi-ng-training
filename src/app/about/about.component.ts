b175640a9a08ba176f0b8c81ce29eaf3d0021350
LP Shivan <lp.shivan@gmail.com>
2017-08-29 11:12:29 +0530

    First commit for Cgi ng training

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-about',
+    // template: `<h3>Directly written templates</h3>
+    //             <p>One more content</p>
+    //             <h3>About template </h3>`,
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.css']
+})
+export class AboutComponent {
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeesComponent } from './employees/employees.component';
+import { AboutComponent } from "./about/about.component";
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeesComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
